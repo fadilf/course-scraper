6da0224e6c299599fa219d93de85267b70149689
Fadil Eledath <fadilfaizal@gmail.com>
2022-12-28 23:51:21 +0530

    Changed search to work through WASM

--- explorer/script.js
+++ explorer/script.js
@@ -1,108 +1,46 @@
-let courseRows = null;
+import init, { setup, render_results } from "../pkg/json_search.js";
 
-$.getJSON("../collated.json", function (data) {
-    let table_content = "";
-    let codes = {};
-    courseList = Object.keys(data.Credits);
-    courseList.forEach((course, index) => {
-        let code = course.split(" ")[0];
-        if (!(code in codes)) {
-            codes[code] = 1;
-            $("#subject-code").append(`<option value="${code}">${code}</option>`);
+init().then(() => {
+    let jsonLoadPromise = setup(new URL("../collated_min.json", document.baseURI).href);
+
+    let filters = {},
+        $academicSemester = $("#academic-semester"),
+        $keywords = $("#query"),
+        $subjectCode = $('#subject-code'),
+        $distributionGroup = $('#distribution-group'),
+        $diversity = $('#diversity'),
+        $courseRows = $("#course-rows");
+
+    function updateFilters() {
+        filters = {
+            "Academic Semester": parseInt($academicSemester.val()),
+            "Keywords": $keywords.val().trim().toUpperCase(),
+            "Subject Code": $subjectCode.val(),
+            "Distribution Group": $distributionGroup.val(),
+            "Analyzing Diversity": $diversity.prop("checked"),
         }
-        let title = data.Title[course];
-        let courseURL = data.URL[course];
-        let distribution = data["Distribution Group"][course];
-        distribution = distribution ? distribution : "";
-        let diversity = data["Analyzing Diversity"][course];
-        diversity = diversity ? diversity : "";
-        let credits = data.Credits[course];
-        table_content +=
-            `<tr class="visible valid-sem valid-query ${index % 2 ? "odds" : "evens"}" data-course="${course}" data-course-code="${code}" data-distribution="${distribution}" data-diversity="${diversity}">
-                <th scope="row">
-                    <a href="https://courses.rice.edu${courseURL}" target="_blank" class="text-decoration-none">
-                        ${course}
-                    </a>
-                </th>
-                <td>${title}</td>
-                <td>${distribution}</td>
-                <td>${diversity}</td>
-                <td>${credits}</td>
-            </tr>`;
-    });
-    $("#course-rows").append(table_content);
-
-    let semesters = Object.keys(data).slice(5);
-    semesters.forEach(semester => {
-        $("#academic-semester").append(`
-        <option value="${semester}">${semester}</option>
-        `);
-    });
-
-    $("#academic-semester").on('change', function () {
-        $("#course-rows tr.valid-sem").removeClass("valid-sem");
-        if (this.value == "") {
-            $("#course-rows tr").addClass("valid-sem");
-        } else {
-            for (const [course, valid] of Object.entries(data[this.value])) {
-                if (valid) {
-                    $(`#course-rows tr[data-course="${course}"]`).addClass("valid-sem");
-                }
-            }
-        }
-        updateVisible();
-    });
-
-    let queryTimeout = null;
-    function executeQuery(queryClean) {
-        $("#course-rows tr.valid-sem").removeClass("valid-query");
-        for (const [course, title] of Object.entries(data.Title)) {
-            if ([course, title].join(" ").indexOf(queryClean) != -1) {
-                $(`#course-rows tr[data-course="${course}"]`).addClass("valid-query");
-            }
-        }
-        updateVisible();
+        $courseRows.html(render_results(filters));
     }
-    function queryPipeline(queryClean) {
-        clearTimeout(queryTimeout);
-        queryTimeout = setTimeout(executeQuery, 400, queryClean);
-    }
-    $("#query").on('input', function () {
-        queryPipeline(this.value.trim().toUpperCase());
+
+    $academicSemester
+        .add($subjectCode)
+        .add($distributionGroup)
+        .add($diversity)
+        .on('change', function () {
+            updateFilters();
+        });
+
+    $keywords.on('input', function () {
+        updateFilters();
     });
 
-    $('#subject-code').on('change', function () {
-        updateVisible();
+    jsonLoadPromise.then(function(data){
+        data[0].forEach(code => {
+            $subjectCode.append(`<option value="${code}">${code}</option>`);
+        });
+        data[1].forEach((semester, idx) => {
+            $academicSemester.append(`<option value="${idx}">${semester}</option>`);
+        });
+        updateFilters();
     });
-
-    $('#distribution-group').on('change', function () {
-        updateVisible();
-    });
-
-    $('#diversity').on('change', function () {
-        updateVisible();
-    });
-
 });
-
-function updateVisible(){
-    let filters = "";
-    let subjectCode = $('#subject-code').val();
-    let distribution = $('#distribution-group').val();
-    let diversity = $("#diversity").prop("checked");
-
-    if (subjectCode != "") {
-        filters += `[data-course-code="${subjectCode}"]`;
-    }
-    if (distribution != "") {
-        filters += `[data-distribution="${distribution}"]`;
-    }
-    if (diversity) {
-        filters += `[data-diversity="Analyzing Diversity Course"]`;
-    }
-
-    $("#course-rows tr.visible").removeClass(["visible", "odds", "evens"]);
-    $(`#course-rows tr.valid-sem.valid-query${filters}`).addClass("visible").each(function(index){
-        $(this).addClass(index % 2 ? "odds" : "evens");
-    });
-}
